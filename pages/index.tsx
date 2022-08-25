307be7ea5bd7151b7e16dbd6eda48b8f2d82494b
Milad Nekofar <milad@nekofar.com>
2022-08-25 13:21:58 +0430

    refactor: add some text to the main index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +9,15 @@ const Home: NextPage = () => {
         <meta name="description" content="Council of Lil Nouns" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
-  )
-}
 
-export default Home
+      <main className="flex h-screen justify-center items-center">
+        <div className="m-auto">
+          <h1 className="text-3xl">Lil Nouncil</h1>
+          <p className="text-xl">We are building something!</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
