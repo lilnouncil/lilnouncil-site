2cabac1bff7e84540590fc8f528406547c807262
Milad Nekofar <milad@nekofar.com>
2023-02-09 12:21:02 +0330

    perf: optimize fonts and remove external network requests
    fixup! perf: optimize fonts and remove external network requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import {Londrina_Solid} from '@next/font/google';
+import clsx from "clsx";
+
+const londrinaSolid = Londrina_Solid({
+  subsets: ['latin'],
+  variable: '--font-londrina-solid',
+  weight: ['100', '300', '400', '900']
+})
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +18,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex h-screen justify-center items-center">
+      <main className={clsx(londrinaSolid.variable, "flex h-screen justify-center items-center font-sans")}>
         <div className="m-auto">
           <h1 className="text-3xl">Lil Nouncil</h1>
           <p className="text-xl">We are building something!</p>
